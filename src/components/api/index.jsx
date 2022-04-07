031ce6efefcabfb08aedcedd5dc1e8aa2f48b08f
Achmad Miftahudin <93859870+myth2310@users.noreply.github.com>
2022-04-08 06:55:31 +0700

    Add Playlistform
    Add Playlistform

---
+++ src/components/api/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './playlist-form.css';
+import { Redirect } from 'react-router-dom';
+
+function PlaylistForm({}){
+	// if(!authorized){
+	// 	return <Redirect to="/"/>
+	// }
+	return (
+		<form className="forms">
+			<div className="playlist-form">
+				<div className="label-input-container">
+					<h3 size="title">Create Playlist</h3>
+					<label htmlFor="title">Title </label>
+					<input
+						id="title"
+						type="text"
+						className="input"
+					/>
+					<label htmlFor="description">Description </label>
+					<textarea
+						id="description"
+						type="text"
+						className="input description"		
+					/>
+					<input type="submit" className="submit-playlist" />
+				</div>
+			</div>
+		</form>
+	);
+};
+
+export default PlaylistForm;
