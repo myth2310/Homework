3fdbd6979ec1143e1d10aa0a2c4eed65dffeacd1
Achmad Miftahudin <93859870+myth2310@users.noreply.github.com>
2022-04-11 08:29:03 +0700

    Update App.js
    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-// import Home from './pages/home/index';
 import React from "react";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-// import Login from './components/api/login/index';
 import PageLogin from './components/api/login/login';
 import Playlist from './components/api/index';
 
 function App() {
 	return (
-		
 		<>
-			<BrowserRouter>
+		<BrowserRouter>
 			<Routes>
 				<Route exact path="/" element={<PageLogin/>}/>
 				<Route path="/playlist" element={<Playlist/>}/>
 				<Route path="*" element={<ErrorPage/>}/>
 			</Routes>
-			</BrowserRouter>
-     
-// 			<Home />
+		</BrowserRouter>
 		</>
 	);
 }
