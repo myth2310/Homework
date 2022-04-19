3b5bfabd828b6ed7704d613dd7079457ad9256d8
Achmad Miftahudin <93859870+myth2310@users.noreply.github.com>
2022-04-20 05:44:27 +0700

    Implemation Testing
    Implemation Testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Sample as NetworkCallSample } from "./pages/search/index";
+import Card from '../src/components/table/table-body/index'
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders PlaceholderText', () => {
+  render(<NetworkCallSample />);
+  const inputNode = screen.getByPlaceholderText('Searce')
+
+  expect(inputNode).toBeInTheDocument();
+});
+
+test('renders button', () => {
+  render(<Card />);
+  const ButtonSelect = screen.getByText('Select');
+  const ButtonDeselect = screen.getByText('Deselect');
+
+  expect(ButtonSelect).toBeInTheDocument();
+  expect(ButtonDeselect).toBeInTheDocument();
 });
